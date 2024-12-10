a001fd5866ab18dbe8d36bafc3fbe1acdc823e24
momen mahmoud <151007734+mo485485wdscxj@users.noreply.github.com>
2024-12-10 22:01:22 +0200

    Update UseNowPage.dart

--- lib/UseNowPage.dart
+++ lib/UseNowPage.dart
@@ -2,9 +2,26 @@ import 'package:flutter/material.dart';
 
 class UseNowPage extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
-  }
+  _UseNowPageState createState() => _UseNowPageState();
+}
 
+class _UseNowPageState extends State<UseNowPage> {
+  final GlobalKey<QrCodeScannerState> qrKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Use Now')),
+      body: Center(
+        child: QRView(
+          key: qrKey,
+          onQRViewCreated: (controller) {
+            controller.scannedDataStream.listen((scanData) {
+              // Handle scan data
+            });
+          },
+        ),
+      ),
+    );
+  }
 }
